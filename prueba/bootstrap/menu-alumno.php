a7d191a549ee188c6ddc37023f5f2a1e2ed8b4ae
Ani26 <annimask@gmail.com>
2014-11-13 12:46:10 -0600

    Practicas de clases

---
+++ prueba/bootstrap/menu-alumno.php
@@ -0,0 +1,8 @@
+<div class="navbar-collapse collapse">
+    <ul class="nav navbar-nav">
+        <li><a href="verMaterias.php?idu=$idu">Materias</a></li>
+
+    </ul>
+    </li>
+    </ul>
+</div><!--/.nav-collapse -->
---
+++ prueba/menu-alumno.php
@@ -0,0 +1,8 @@
+<div class="navbar-collapse collapse">
+    <ul class="nav navbar-nav">
+        <li class="active"><a href="index3.php">Inicio</a></li>
+        <li><a href="TestAlumno.php?idu=$idu">ver materias</a></li>
+    </ul>
+    </li>
+    </ul>
+</div><!--/.nav-col
